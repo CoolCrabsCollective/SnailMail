313de2dd7d5e90f15e2547cc490399a65a56f9b0
William Wells <williamwells2013@hotmail.com>
2023-04-29 12:50:53 -0400

    Uncomment broken line

--- client/src/input/EntitySelection.cpp
+++ client/src/input/EntitySelection.cpp
@@ -10,9 +10,9 @@ EntitySelection::EntitySelection(World &world) : world(world) {
 void EntitySelection::clickScan(sf::Vector2f clickPos) {
     const std::map<ZOrder, std::vector<Entity*>> zOrderMap = world.getZOrderMap();
 
-    for(int i = 0; i < ZOrder::ENUM_LENGTH; i++) {
-        for(Entity* entity: zOrderMap[static_cast<ZOrder>(i)]) {
-
-        }
-    }
+//    for(int i = 0; i < ZOrder::ENUM_LENGTH; i++) {
+//        for(Entity* entity: zOrderMap[static_cast<ZOrder>(i)]) {
+//
+//        }
+//    }
 }
