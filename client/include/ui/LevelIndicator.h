fe16642a593562f832be7595798f5a5210e9bf5d
William Wells <williamwells2013@hotmail.com>
2023-04-30 15:11:33 -0400

    Finish level indicator

--- client/include/ui/LevelIndicator.h
+++ client/include/ui/LevelIndicator.h
@@ -7,11 +7,23 @@
 
 #include "SFML/Graphics/Drawable.hpp"
 #include "SFML/Graphics/Sprite.hpp"
+#include "SFML/Graphics/Text.hpp"
 
 class World;
 
 class LevelIndicator : public sf::Drawable {
     mutable sf::Sprite backgroundSprite;
+    mutable sf::Text label;
+
+    mutable sf::Text level_num;
+
+    sf::Vector2f mainOffset = {50.0f, 55.0f};
+    sf::Vector2f numOffset = {-10.f, -20.f};
+
+    sf::Vector2f labelOffset = {-30.f, -60.f};
+
+    std::string levelNum;
+
     World& world;
 
 public:
--- client/src/ui/LevelIndicator.cpp
+++ client/src/ui/LevelIndicator.cpp
@@ -9,12 +9,32 @@
 
 LevelIndicator::LevelIndicator(World &world) : world(world) {
     backgroundSprite.setTexture(*world.getAssets().get(GameAssets::LEVEL_INDICATOR));
-    backgroundSprite.setPosition({50.0f, 50.0f});
+    backgroundSprite.setPosition(mainOffset);
+
+    level_num.setFont(*world.getAssets().get(GameAssets::THE_RIGHT_FONT));
+    level_num.setFillColor(sf::Color::Black);
+    level_num.setPosition(mainOffset + numOffset);
+
+    label.setFont(*world.getAssets().get(GameAssets::THE_RIGHT_FONT));
+    label.setFillColor(sf::Color::Black);
+    label.setPosition(mainOffset + labelOffset);
+    label.setString("LEVEL");
 }
 
 void LevelIndicator::draw(sf::RenderTarget &target, const sf::RenderStates &states) const {
     SpriteUtil::setSpriteSize(backgroundSprite, sf::Vector2f{50.f, 50.f});
     SpriteUtil::setSpriteOrigin(backgroundSprite, sf::Vector2f{0.5f, 0.5f});
-
     target.draw(backgroundSprite);
+
+
+    std::string levelNum = std::to_string(world.getCurrentLevelNumber());
+
+    if (levelNum.length() < 2) {
+        levelNum.insert(0, "0");
+    }
+    level_num.setString(levelNum);
+    target.draw(level_num);
+
+
+    target.draw(label);
 }
