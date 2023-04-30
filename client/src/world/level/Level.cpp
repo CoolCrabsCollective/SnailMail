3f963d66f20c456d424628459a0cc01824176465
Cedric Martens <cedricmartens98@gmail.com>
2023-04-30 12:14:39 -0400

    A

--- client/src/world/level/Level.cpp
+++ client/src/world/level/Level.cpp
@@ -6,29 +6,26 @@
 const Level Level::LEVELS[] = {
         {
             .seeded = true,
-            .seed = 777,
-            .nodeCount = 6,
-            .friends = { { LADYBUG, false, 2 },
-                         { FROG, false, 3 }},
+            .seed = 123,
+            .nodeCount = 3,
+            .friends = { { LADYBUG, false, 2 }},
             .offices = { { false, 0 }},
-            .snailCount = 2,
-            .missions = { { 1.0f, 0, 0, { { 0, false, 10.0f }, { 1, true, 15.0f } } }}
+            .snailCount = 1,
+            .missions = { { 1.0f, 0, 0, { { 0, false, 10.0f }, }}}
         },
         {
             .seeded = true,
-            .seed = 666,
-            .nodeCount = 10,
+            .seed = 2,
+            .nodeCount = 5,
             .friends = { { LADYBUG, false, 2 },
-                         { FROG, false, 3 },
-                         { MOUSE, false, 5 },
-                         { BEE, false, 9 } },
+                         { FROG, false, 3 }},
             .offices = { { false, 0 }},
             .snailCount = 1,
-            .missions = { { 1.0f, 0, 0,
-                            { { 0, true, 10.0f },
-                              { 1, true, 20.0f },
-                              { 2, true, 30.0f },
-                              { 3, true, 50.0f }} } }
+            .missions = {{1.0f, 0, 0,
+                          {{0, true, 15.0f},
+                           {1, true, 20.0f},
+                          },}
+            }
         },
         {
                 .seeded = true,
